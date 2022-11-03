9e375e8e57df3fa8ac1b380f79f2bebb5a34b1f3
George Lopatenko <81328772+Lopa10ko@users.noreply.github.com>
2022-11-03 14:01:53 +0300

    fix: resolve whitespace problem

--- labs/blab1/x.sh
+++ labs/blab1/x.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-man bash | grep -o -i "[[:alnum:]]\{4,\}" | tr "[[:upper:]]" "[[:lower:]]" | sort |uniq -c | sort -n -r | head -3
+man bash | grep -o -i "[[:alnum:]]\{4,\}" | tr "[[:upper:]]" "[[:lower:]]" | sort | uniq -c | sort -n -r | head -3
 exit
